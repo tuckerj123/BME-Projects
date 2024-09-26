9cb1cbe39ad16c13b9a4af0d9b8834b4f067f9ae
seagullzzzz <ryancgl26@gmail.com>
2024-09-26 11:31:53 -0400

    In Class work 9-26

--- project1_module.py
+++ project1_module.py
@@ -6,6 +6,7 @@ Created on Tue Sep 24 11:13:13 2024
 @author: tuckerjohnsen
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load_data_arrays(input_file):
 
@@ -20,3 +21,47 @@ def load_data_arrays(input_file):
     electrode = data['electrode']
     units = data['units']
     return ecg_voltage, fs, label_samples, label_symbols, subject_id, electrode, units
+
+def plot_raw_data(signal_voltage, signal_time, units="V", title=""):
+    # Plot data
+    plt.figure(1, clear=True)
+    plt.plot(signal_time, signal_voltage)
+
+    #annotate
+    plt.xlabel('Time (s)')
+    plt.ylabel(f"Voltage ({units})") # arbitrary units
+    plt.grid()
+    plt.title(title)
+    
+def plot_events(label_samples, label_symbols, signal_time, signal_voltage):
+    
+    #divide samples by the labels in event_types
+    event_types = np.unique(label_symbols)
+    event_samples_N = label_samples[label_symbols == event_types[0]]
+    event_samples_V = label_samples[label_symbols == event_types[1]]
+    
+    #seperate times by the samples
+    event_times_N = signal_time[event_samples_N]
+    event_times_V = signal_time[event_samples_V]
+    
+    #seperate voltages by the samples
+    event_signals_N = signal_voltage[event_samples_N]
+    event_signals_V = signal_voltage[event_samples_V]
+
+    
+    print(len(event_samples_N))
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- project1_script.py
+++ project1_script.py
@@ -5,21 +5,34 @@ Created on Tue Sep 24 10:38:29 2024
 
 @author: tuckerjohnsen
 """
-
 # Import Data
+
 # Import Packages
 import numpy as np
-from matplotlib import pyplot as py
-from project1_module import load_data_arrays as lda
+from matplotlib import pyplot as plt
+import project1_module as p1m
 
 # Loading data into arrays
 input_file = 'ecg_e0103_half1.npz'
     
-ecg_voltage, fs, label_samples, label_symbols, subject_id, electrode, units = lda(input_file)
+ecg_voltage, fs, label_samples, label_symbols, subject_id, electrode, units = p1m.load_data_arrays(input_file)
 
 # convert sample array to time
 
 samples = np.arange(len(ecg_voltage))
 t = 1/fs * samples
 
-#%% New Module 
+#%% Plot Raw signal
+
+p1m.plot_raw_data(ecg_voltage, t, title=f"ECG Raw Data for Subject {subject_id}")
+
+#%%
+import numpy as np
+from matplotlib import pyplot as plt
+import project1_module as p1m
+
+p1m.plot_events(label_samples, label_symbols, t, ecg_voltage)
+
+
+
+
